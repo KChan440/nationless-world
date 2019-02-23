eeba3044ca8130aa4f075ee766693b069dc995fc
Kumaran <kumaranc46@gmail.com>
2019-02-23 16:34:26 -0500

    black margin

--- src/site/components/Landing.jsx
+++ src/site/components/Landing.jsx
@@ -5,10 +5,19 @@ import {Navbar, Nav,} from 'react-bootstrap/';
 import styled from 'styled-components';
 import '../style/landing.css';
 
+
+const BottomBar = styled.div`
+background-color: black;
+height: 10%;
+position: fixed;
+bottom: 0;
+width: 100%;
+`;
 export default class Landing extends Component{
     render(){
         return(
-            <Navbar bg="dark" variant="dark">
+            <div>
+            <Navbar className="black" bg="dark" variant="dark">
             <Navbar.Brand href="#home">Nationless World</Navbar.Brand>
             <Navbar.Collapse>
             <Nav className="ml-auto">
@@ -17,6 +26,8 @@ export default class Landing extends Component{
             </Nav>
             </Navbar.Collapse>
             </Navbar>
+            <BottomBar/>
+            </div>
         )
     }
 }
